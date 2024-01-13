58e4c5c4a6fd3dfdc48816ad8b8549fcd45a5023
Revanth1811 <vutukururevanth@gmail.com>
2024-01-13 06:02:16 +0530

    modified

--- OneDrive/Desktop/FullStack/Javascript/Exam center allotment/app.js
+++ OneDrive/Desktop/FullStack/Javascript/Exam center allotment/app.js
@@ -1,13 +1,49 @@
 let students=[]
+let examcenters=['kurnool','hyderabad','Chennai','Banglore','Trivendram','vizag','vijaywada','warangal','tirupati','Kochi']
+
 function allotment(){
     let name=document.getElementById('name');
     let email=document.getElementById('email');
+    let alert=document.getElementById('alert');
+    let sname=document.getElementById('sname');
+    let semail=document.getElementById('semail');
+    let stexamCenter=document.getElementById('stexamCenter');
+    let successNotification=document.getElementById('audio')
+    let errorNotification=document.getElementById('audio1')
 
-    let student={
-        name:name.value,
-        email:email.value
+    function alertAction(){
+        alert.classList.remove('d-none')
+        // alert.classList.add("alertcss")
+        setTimeout(() => {
+            alert.classList.add('d-none')
+        }, 2500);
     }
 
-    students.push(student);
-    console.log(students)
+    let ifUserexits = students.filter((data)=>{
+        return data.email==email.value
+     })
+     
+     if(ifUserexits.length>0){
+          alert.innerText="Examcenter already alloted";
+          errorNotification.play();
+          alertAction();
+     }
+     else{
+         let student={
+             name:name.value,
+             email:email.value
+         }
+         students.push(student)
+         let random = examcenters[Math.floor(Math.random() * examcenters.length)];
+         alert.innerText="Examcenter alloted successfully";
+         alertAction();
+         successNotification.play();
+         sname.innerText=name.value;
+         semail.innerText=email.value;
+         stexamCenter.innerText=random;
+         console.log(students)
+         console.log("Exam Center : "+random)
+         
+     }
 }
+ 
